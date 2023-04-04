d50647cc6c29d54f66a3da65e3d1c9f615d405d4
Balajiw920 <wbalaji2021@gmail.com>
2023-04-05 01:15:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+const output = document.getElementById("output");
+
+const promises = [];
+for (let i = 1; i <= 5; i++) {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const rand = Math.random();
+      if (rand < 0.5) {
+        reject(`Promise ${i} rejected with error`);
+      } else {
+        resolve(`Promise ${i} resolved with ${Math.floor(Math.random() * 10) + 1}`);
+      }
+    }, 1000); // wait 1 second
+  });
+  promises.push(promise);
+}
+
+Promise.all(promises)
+  .then((results) => {
+    results.forEach((result) => {
+      const p = document.createElement("p");
+      p.innerText = result;
+      output.appendChild(p);
+    });
+  })
+  .catch((error) => {
+    const p = document.createElement("p");
+    p.innerText = error;
+    output.appendChild(p);
+  });
