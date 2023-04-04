e945780b93c621e36693d0e46bb923963e6e31b4
Balajiw920 <wbalaji2021@gmail.com>
2023-04-05 01:18:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,28 @@
-//your JS code here. If required.
 const output = document.getElementById("output");
 
-const promises = [];
-for (let i = 1; i <= 5; i++) {
-  const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const rand = Math.random();
-      if (rand < 0.5) {
-        reject(`Promise ${i} rejected with error`);
-      } else {
-        resolve(`Promise ${i} resolved with ${Math.floor(Math.random() * 10) + 1}`);
-      }
-    }, 1000); // wait 1 second
-  });
-  promises.push(promise);
-}
+// Create an array of five promises, each with a 50% chance of resolving to a random number between 1 and 10, or rejecting with an error
+const promises = Array.from({ length: 5 }, () => {
+  const randomNumber = Math.floor(Math.random() * 10) + 1;
+  if (randomNumber % 2 === 0) {
+    return Promise.resolve(randomNumber);
+  } else {
+    return Promise.reject(`Promise ${randomNumber} rejected with error`);
+  }
+});
 
-Promise.all(promises)
+// Wait for all promises to settle and then log the results or errors to the output div
+Promise.allSettled(promises)
   .then((results) => {
-    results.forEach((result) => {
+    results.forEach((result, index) => {
       const p = document.createElement("p");
-      p.innerText = result;
+      if (result.status === "rejected") {
+        p.textContent = result.reason;
+      } else {
+        p.textContent = `Promise ${index + 1} resolved with ${result.value}`;
+      }
       output.appendChild(p);
     });
   })
   .catch((error) => {
-    const p = document.createElement("p");
-    p.innerText = error;
-    output.appendChild(p);
+    console.error(error);
   });
