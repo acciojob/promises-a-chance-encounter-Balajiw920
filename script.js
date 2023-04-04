cefb9fafdc6f67d124689e5abd33ec0d16aa10b4
Balajiw920 <wbalaji2021@gmail.com>
2023-04-05 01:21:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
 const output = document.getElementById("output");
 
-// Create an array of five promises, each with a 50% chance of resolving to a random number between 1 and 10, or rejecting with an error
-const promises = Array.from({ length: 5 }, () => {
-  const randomNumber = Math.floor(Math.random() * 10) + 1;
-  if (randomNumber % 2 === 0) {
-    return Promise.resolve(randomNumber);
-  } else {
-    return Promise.reject(`Promise ${randomNumber} rejected with error`);
-  }
-});
+      // Create an array of five promises
+      const promises = Array.from({ length: 5 }, (_, i) => {
+        return new Promise((resolve, reject) => {
+          // Randomly decide whether to resolve or reject the promise
+          const shouldResolve = Math.random() < 0.5;
+          if (shouldResolve) {
+            // Resolve the promise with a random number between 1 and 10
+            const randomNumber = Math.floor(Math.random() * 10) + 1;
+            resolve(randomNumber);
+          } else {
+            // Reject the promise with an error message
+            reject(`Promise ${i + 1} rejected with error`);
+          }
+        });
+      });
 
-// Wait for all promises to settle and then log the results or errors to the output div
-Promise.allSettled(promises)
-  .then((results) => {
-    results.forEach((result, index) => {
-      const p = document.createElement("p");
-      if (result.status === "rejected") {
-        p.textContent = result.reason;
-      } else {
-        p.textContent = `Promise ${index + 1} resolved with ${result.value}`;
-      }
-      output.appendChild(p);
-    });
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+      // Use Promise.all to wait for all promises to settle, and then log the array of results or errors
+      Promise.allSettled(promises).then((results) => {
+        results.forEach((result) => {
+          // Check whether the promise resolved or rejected, and log the appropriate message
+          if (result.status === "fulfilled") {
+            const value = result.value;
+            output.innerHTML += `<p>${value}</p>`;
+          } else {
+            const reason = result.reason;
+            output.innerHTML += `<p>${reason}</p>`;
+          }
+        });
+      });
